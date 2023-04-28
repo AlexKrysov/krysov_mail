2937edf2b8f9d3d5a12b03fbc688e4d70262f79f
Alex Krysov <krysov-95@mail.ru>
2023-04-28 09:49:38 +0600

    fix test

--- src/test/java/com/krysov/tests/TestBase.java
+++ src/test/java/com/krysov/tests/TestBase.java
@@ -12,7 +12,6 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestBase {
 
@@ -24,16 +23,7 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.browserVersion = System.getProperty("browserVersion", "100");
-        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
-        Configuration.remote = System.getProperty("selenoidUrl", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
-        Configuration.timeout = 10000;
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", true);
-        Configuration.browserCapabilities = capabilities;
+        Configuration.browserSize = "1920x1080";
     }
 
     @BeforeEach
